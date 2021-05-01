ae34697851f4ad5fe78b5b7b6fef59e28f2631ac
Thales Fragoso <thales.fragosoz@gmail.com>
2021-05-01 19:23:23 -0300

    Initial sdmmc_v2 support

--- parse.py
+++ parse.py
@@ -217,6 +217,7 @@ perimap = [
     ('.*:RNG:rng1_v3_1', 'rng_v1/RNG'),
     ('STM32F4.*:SYS:.*', 'syscfg_f4/SYSCFG'),
     ('STM32L4.*:SYS:.*', 'syscfg_l4/SYSCFG'),
+    ('.*SDMMC:sdmmc2_v1_0', 'sdmmc_v2/SDMMC'),
 ]
 
 
