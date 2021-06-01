375ddbc3a90c9d77b76430d2385a571e8be81b0c
Bob McWhirter <bmcwhirt@redhat.com>
2021-06-01 14:41:12 -0400

    Extract and clean ADC v3. Adjust paths for extract.sh.

--- extract.sh
+++ extract.sh
@@ -6,7 +6,7 @@ board=$1
 peri=$2
 mkdir -p regs/$peri
 
-cargo build --release --manifest-path ../../../svd2rust/Cargo.toml 
+cargo build --release --manifest-path ../../svd2rust/Cargo.toml 
 
 transform="transform.yaml"
 
@@ -28,7 +28,7 @@ for f in `ls $query`; do
     f=${f#"stm32"}
     f=${f%".svd"}
     echo -n processing $f ...
-    RUST_LOG=info ../../../svd2rust/target/release/svd4rust extract-peripheral --svd sources/svd/stm32$f.svd --transform $transform --peripheral $peri > regs/$peri/$f.yaml 2> regs/$peri/$f.yaml.out
+    RUST_LOG=info ../../svd2rust/target/release/svd4rust extract-peripheral --svd sources/svd/stm32$f.svd --transform $transform --peripheral $peri > regs/$peri/$f.yaml 2> regs/$peri/$f.yaml.out
     if [ $? -ne 0 ]; then 
         mv regs/$peri/$f.yaml.out regs/$peri/$f.err
         rm regs/$peri/$f.yaml
