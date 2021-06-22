6ca1d259f953a3d934b114dbb9dbb16fcc3adf98
Thales Fragoso <thales.fragosoz@gmail.com>
2021-06-21 23:15:40 -0300

    Add F0 FLASH

--- parse.py
+++ parse.py
@@ -330,6 +330,7 @@ perimap = [
     ('.*SDMMC:sdmmc2_v1_0', 'sdmmc_v2/SDMMC'),
     ('.*:STM32H7_pwr_v1_0', 'pwr_h7/PWR'),
     ('.*:STM32H7_flash_v1_0', 'flash_h7/FLASH'),
+    ('.*:STM32F0_flash_v1_0', 'flash_f0/FLASH'),
     ('.*TIM\d.*:gptimer.*', 'timer_v1/TIM_GP16'),
     ('.*ETH:ethermac110_v3_0', 'eth_v2/ETH'),
 
