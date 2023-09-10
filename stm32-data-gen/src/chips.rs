bca8d0d824db872bec09d5fdeb707cf34be4143d
Guangzong Chen <chen-gz@outlook.com>
2023-09-10 14:40:34 -0400

    update sdmmc

--- stm32-data-gen/src/chips.rs
+++ stm32-data-gen/src/chips.rs
@@ -247,7 +247,8 @@ impl PeriMatcher {
             (".*:SAI:sai1_v2_1", ("sai", "v4", "SAI")),
             (".*:SDIO:sdmmc_v1_2", ("sdmmc", "v1", "SDMMC")),
             (".*:SDMMC:sdmmc_v1_3", ("sdmmc", "v1", "SDMMC")),
-            ("STM32U5.*:SDMMC:.*", ("sdmmc", "v2", "SDMMC")),
+            (".*SDMMC:sdmmc2_v1_0", ("sdmmc", "v2", "SDMMC")),
+            ("STM32U5.*:SDMMC:sdmmc2_v2_1", ("sdmmc", "v2", "SDMMC")),
             (".*:SPDIFRX:spdifrx1_v1_0", ("spdifrx", "v1", "SPDIFRX")),
             // # USB
             ("STM32(F1|L1).*:USB:.*", ("usb", "v1", "USB")),
@@ -309,7 +310,6 @@ impl PeriMatcher {
             ("STM32G0C1.*:CRS:.*", ("crs", "v1", "CRS")),
             ("STM32G4.*:CRS:.*", ("crs", "v1", "CRS")),
             ("STM32U5.*:CRS:.*", ("crs", "v1", "CRS")),
-            (".*SDMMC:sdmmc2_v1_0", ("sdmmc", "v2", "SDMMC")),
             ("STM32C0.*:PWR:.*", ("pwr", "c0", "PWR")),
             ("STM32G0.*:PWR:.*", ("pwr", "g0", "PWR")),
             ("STM32G4.*:PWR:.*", ("pwr", "g4", "PWR")),
