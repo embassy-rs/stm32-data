e7a291e659cc1b3a264f7ceb22bcdb78059519e8
xoviat <xoviat@users.noreply.github.com>
2023-10-05 20:04:58 -0500

    sort pins by key

--- stm32-data-gen/src/chips.rs
+++ stm32-data-gen/src/chips.rs
@@ -1017,7 +1017,7 @@ fn process_core(
         }
 
         // sort pins to avoid diff for c pins
-        p.pins.sort();
+        p.pins.sort_by_key(|x| (x.pin.clone(), x.signal.clone()));
 
         if let Some(peri_irqs) = chip_irqs.get(&pname) {
             use stm32_data_serde::chip::core::peripheral::Interrupt;
