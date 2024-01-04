f7ad1d5b0fed1bdd02f00c96abb956cee6eba229
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2024-01-03 18:09:28 -0600

    Add icache_v1 for use with STM32U5

--- stm32-data-gen/src/chips.rs
+++ stm32-data-gen/src/chips.rs
@@ -513,6 +513,7 @@ impl PeriMatcher {
             ),
             ("STM32L4.*:GFXMMU:.*", ("gfxmmu", "v1", "GFXMMU")),
             ("STM32U5.*:GFXMMU:.*", ("gfxmmu", "v2", "GFXMMU")),
+            ("STM32U5.*:ICACHE:.*", ("icache", "v1", "ICACHE")),
             ("STM32F0x[128].*:TSC:.*", ("tsc", "v1", "TSC")),
             ("STM32F3[07][123].*:TSC:.*", ("tsc", "v1", "TSC")),
             ("STM32WB55.*:TSC:.*", ("tsc", "v2", "TSC")),
