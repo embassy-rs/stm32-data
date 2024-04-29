e001b7dfcda0f2e0ba68cf4ab2ab2f0320abe162
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-04-29 18:33:48 +0200

    Add U5 LPDMA.

--- stm32-data-gen/src/dma.rs
+++ stm32-data-gen/src/dma.rs
@@ -300,6 +300,7 @@ impl DmaChannels {
             ("H5_GPDMA.yaml", "GPDMA1", "STM32H5_dma3_Cube", 8, 2),
             ("H5_GPDMA.yaml", "GPDMA2", "Instance2_STM32H5_dma3_Cube", 8, 2),
             ("U5_GPDMA1.yaml", "GPDMA1", "STM32U5_dma3_Cube", 16, 4),
+            ("U5_LPDMA.yaml", "LPDMA1", "STM32U5_dma3_Cube", 4, 0),
             ("WBA_GPDMA1.yaml", "GPDMA1", "STM32WBA_dma3_Cube", 8, 0),
         ] {
             let mut chip_dma = ChipDma {
