75952a08c9f5491aeaa044d390f14678f15e67b9
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2025-01-10 18:51:18 +0100

    Fix copypaste screwup.

--- stm32-metapac-gen/res/src/lib.rs
+++ stm32-metapac-gen/res/src/lib.rs
@@ -5,7 +5,7 @@
 #![doc(html_no_source)]
 #![cfg_attr(
     docsrs,
-    doc = "<div style='padding:30px;background:#810;color:#fff;text-align:center;'><p>You might want to <a href='https://docs.embassy.dev/stm32-metapac'>browse the `embassy-nrf` documentation on the Embassy website</a> instead.</p><p>The documentation here on `docs.rs` is built for a single chip only (stm32h755zi-cm7 in particular), while on the Embassy website you can pick your exact chip from the top menu. Available peripherals and their APIs change depending on the chip.</p></div>\n\n"
+    doc = "<div style='padding:30px;background:#810;color:#fff;text-align:center;'><p>You might want to <a href='https://docs.embassy.dev/stm32-metapac'>browse the `stm32-metapac` documentation on the Embassy website</a> instead.</p><p>The documentation here on `docs.rs` is built for a single chip only (stm32h755zi-cm7 in particular), while on the Embassy website you can pick your exact chip from the top menu. Available peripherals and their APIs change depending on the chip.</p></div>\n\n"
 )]
 #![doc = include_str!("../README.md")]
 
