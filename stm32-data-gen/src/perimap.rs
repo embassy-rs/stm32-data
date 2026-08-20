91df0df96c390d2df1dacf6fea0367c87752b59d
Leon Andrea Loeser <info@leon-loeser.de>
2026-08-20 22:33:07 +0200

    Start on specific dfsdm variations

--- stm32-data-gen/src/perimap.rs
+++ stm32-data-gen/src/perimap.rs
@@ -851,7 +851,12 @@ const PERIMAP: &[(&str, (&str, &str, &str))] = &[
     (".*:XSPIM:XSPIM:xspi_v2_1H7RS*", ("xspim", "v1", "XSPIM")),
     ("STM32H7.*:MDMA:.*", ("mdma", "v1", "MDMA")),
     //TODO DFSDM
-    ("STM32H7[AB].*:DFSDM:.*", ("dfsdm", "v1", "DFSDM")),
+    // ("STM32H7[AB].*:DFSDM:.*", ("dfsdm", "v1", "DFSDM_8CH_8FLT_DLY")),
+    (
+        "STM32H7[AB].*:dfsdm1_v1_0_H7A_8ch8f.*",
+        ("dfsdm", "v1", "DFSDM_8CH_8FLT_DLY"),
+    ),
+    ("STM32H7[AB].*:dfsdm1_v1_0_H7A_2ch.*", ("dfsdm", "v1", "DFSDM_2CH_1FLT")),
 ];
 
 pub struct Perimap {
