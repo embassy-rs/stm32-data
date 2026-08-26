9f34fecdb65f36911e3c33221dc76c15a90eb7a9
Leon Andrea Loeser <info@leon-loeser.de>
2026-08-26 14:07:53 +0200

    Add all existing variations, not yet all in perimap

--- stm32-data-gen/src/perimap.rs
+++ stm32-data-gen/src/perimap.rs
@@ -857,9 +857,12 @@ const PERIMAP: &[(&str, (&str, &str, &str))] = &[
     // ("STM32H7[AB].*:DFSDM:.*", ("dfsdm", "v1", "DFSDM_8CH_8FLT_DLY")),
     (
         "STM32H7[AB].*:dfsdm1_v1_0_H7A_8ch8f.*",
-        ("dfsdm", "v1", "DFSDM_8CH_8FLT_DLY"),
+        ("dfsdm", "v1", "DFSDM_8CH_8FLT_DLY_TRG5_ADC"),
     ),
-    ("STM32H7[AB].*:dfsdm1_v1_0_H7A_2ch.*", ("dfsdm", "v1", "DFSDM_2CH_1FLT")),
+    (
+        "STM32H7[AB].*:dfsdm1_v1_0_H7A_2ch.*",
+        ("dfsdm", "v1", "DFSDM_2CH_1FLT_TRG3_ADC"),
+    ),
 ];
 
 pub struct Perimap {
